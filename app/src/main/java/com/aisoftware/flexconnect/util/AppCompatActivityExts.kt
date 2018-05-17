66d1c346dd881a7e9926290b86ac58e172448fc7
Rick Bloyer <rick.bloyer@aexp.com>
2018-05-16 18:14:59 -0700

    Added camera permissions setup and call into camera api

---
+++ app/src/main/java/com/aisoftware/flexconnect/util/AppCompatActivityExts.kt
@@ -0,0 +1,19 @@
+package com.aisoftware.flexconnect.util
+
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AppCompatActivity
+
+fun AppCompatActivity.isPermissionGranted(permission: String) =
+        ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+
+fun AppCompatActivity.shouldShowPermissionRationale(permission: String) =
+        ActivityCompat.shouldShowRequestPermissionRationale(this, permission)
+
+fun AppCompatActivity.requestPermission(permission: String, requestId: Int) =
+        ActivityCompat.requestPermissions(this, arrayOf(permission), requestId)
+
+fun AppCompatActivity.batchRequestPermissions(permissions: Array<String>, requestId: Int) =
+        ActivityCompat.requestPermissions(this, permissions, requestId)
+
+fun IntArray.containsOnly(num: Int): Boolean = filter { it == num }.isNotEmpty()
